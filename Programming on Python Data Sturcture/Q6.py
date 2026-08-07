c7d2250d4b4c491013325a218c5df5c9f3685eae
Sumer Lodhi <sumerl.ml25@sbjit.edu.in>
2026-08-07 15:37:01 +0530

    Question 6

---
+++ Programming on Python Data Sturcture/Q6.py
@@ -0,0 +1,7 @@
+fruits = {"apple", "banana", "cherry"}
+fruits.add("mango")
+print("After adding:", fruits)
+fruits.remove("banana")
+print("After remove():", fruits)
+fruits.discard("apple")
+print("After discard():", fruits)
